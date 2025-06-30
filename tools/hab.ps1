a954a5de316370d719be06162b0b4a287bfbdcd2
coolkiid <73600915+coolkiid@users.noreply.github.com>
2025-06-30 16:18:52 +0800

    [Infra] Upgrade habitat version to 0.3.142
    Fix a permission issue on Windows.

--- tools/hab.ps1
+++ tools/hab.ps1
@@ -1,6 +1,6 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-$__version__ = "0.3.141"
+$__version__ = "0.3.142"
 
 if ($null -eq $env:HABITAT_VERSION) { 
     $HABITAT_VERSION = $__version__
